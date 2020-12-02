c1f0d596ea53c8f2cbc809436ddb352c40c9ee7c
Igor Silva <asilvaigor@gmail.com>
2020-12-02 11:41:47 +0100

    Add C parameter optimization to logistic regression fit

--- src/classifier_chain/ClassifierChain.py
+++ src/classifier_chain/ClassifierChain.py
@@ -1,5 +1,8 @@
-from sklearn.multioutput import ClassifierChain as skClassifierChain
+import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import brier_score_loss
+from sklearn.multioutput import ClassifierChain as skClassifierChain
+from sklearn.utils import check_random_state
 
 from .ExhaustiveSearchInferer import ExhaustiveSearchInferer
 
@@ -8,34 +11,45 @@ class ClassifierChain:
     """Base classifier chain to be used to compare different inference methods.
     """
 
-    def __init__(self, base_estimator=LogisticRegression(), order='random', random_state=0):
+    def __init__(self, base_estimator='logistic_regression', order='random',
+                 random_state=0):
         """Default constructor.
 
         Args:
-            base_estimator (sklearn.base.BaseEstimator, optional): Base estimator for each 
-                node of the chain. Defaults to LogisticRegression().
+            base_estimator (str or sklearn.base.BaseEstimator, optional): Base estimator 
+                for each node of the chain. Defaults to 'logistic_regression'.
             order (str or list, optional): Labels classification order. Defaults to
                 'random'.
             random_state (int, optional): Defaults to 0.
         """
+
+        self.__base_estimator = base_estimator
+        if base_estimator == 'logistic_regression':
+            base_estimator = LogisticRegression()
+
         self.cc = skClassifierChain(
             base_estimator=base_estimator, order=order, random_state=random_state)
 
-    def fit(self, x, y):
+    def fit(self, ds, optimization=True):
         """Fits the base estimators.
 
         Args:
-            x (np.array): Train data of shape (n x d1).
-            y (np.array): Sparse train outputs of shape (n x d2).
+            ds (Dataset): Dataset to fit the chain in.
+            optimization (bool): This activates a precalibration step in the base 
+                estimators to identify their best parameters. Only implemented if 
+                base_estimator = 'logistic_regression'.
         """
 
-        self.cc.fit(x, y)
+        if optimization:
+            self.__optimized_fit(ds)
+        else:
+            self.cc.fit(ds.train_x, ds.train_y)
 
-    def predict(self, x, inference_method, return_num_nodes=False, **kwargs):
+    def predict(self, ds, inference_method, return_num_nodes=False, **kwargs):
         """Predicts the test's labels using a chosen inference method.
 
         Args:
-            x (np.array): Test data of shape (n x d1).
+            ds (Dataset): Dataset to get the test data from.
             inference_method (str): Inference method to be used in the prediction. One of 
                 ['greedy', 'exhaustive_search'].
             return_num_nodes (bool, optional): If it should return the number of visited 
@@ -48,11 +62,12 @@ class ClassifierChain:
         if inference_method == 'greedy':
             # Greedy inference. O(d). Checkout implementation at
             # https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/multioutput.py
-            pred, num_nodes = self.cc.predict(x), len(self.cc.estimators_)
+            pred, num_nodes = self.cc.predict(
+                ds.test_x), len(self.cc.estimators_)
         elif inference_method == 'exhaustive_search':
             # Exhaustive search inference. O(2^d)
             inferer = ExhaustiveSearchInferer(self.cc)
-            pred, num_nodes = inferer.infer(x)
+            pred, num_nodes = inferer.infer(ds.test_x)
         else:
             raise Exception('This inference method does not exist.')
 
@@ -60,3 +75,75 @@ class ClassifierChain:
             return pred, num_nodes
         else:
             return pred
+
+    def __optimized_fit(self, ds):
+        """Calibrates the base estimators parameters and fits them. 
+
+        If base_estimator = 'logistic_regression', it will find the best regularization 
+        parameter C for each individual binary regressor by perform a grid search over the
+        values [0.001, 0.01, 0.1, 1, 10, 100, 1000] optimizing brier loss. Same strategy 
+        used in MENA et al.
+
+        The fit method from sklearn needed to be rewritten because in it the estimators_ 
+        variable is reinitialized every time, so putting specific parameters for each base
+        estimator isn't possible.
+
+        Args:
+            ds (Dataset): Dataset to get the train and test data from.
+        """
+
+        n_estimators = ds.train_y.shape[1]
+        best_estimators = [None for _ in range(n_estimators)]
+        best_score = np.full((n_estimators,), np.inf)
+
+        self.__initialize_order(n_estimators)
+        x_aug = np.hstack((ds.train_x, ds.train_y[:, self.cc.order_]))
+
+        if self.__base_estimator == 'logistic_regression':
+            for C in [0.001, 0.01, 0.1, 1, 10, 100, 1000]:
+                self.cc.estimators_ = [LogisticRegression(
+                    C=C) for _ in range(n_estimators)]
+
+                # Fitting them manually to avoid resetting estimators
+                for chain_idx, estimator in enumerate(self.cc.estimators_):
+                    y = ds.train_y[:, self.cc.order_[chain_idx]]
+                    estimator.fit(
+                        x_aug[:, :(ds.train_x.shape[1] + chain_idx)], y)
+
+                pred = self.cc.predict(ds.test_x)
+                score = np.array([brier_score_loss(ds.test_y[:, i], pred[:, i])
+                                  for i in range(n_estimators)])
+                score = score[self.cc.order_]
+
+                change = score < best_score
+                best_score[change] = score[change]
+                for i in range(len(change)):
+                    if change[i]:
+                        best_estimators[i] = self.cc.estimators_[i]
+
+            self.cc.estimators_ = best_estimators
+        else:
+            self.cc.fit(ds.train_x, ds.train_y)
+
+    def __initialize_order(self, n_estimators):
+        """Initializes order_ variable.
+
+        Copied from 
+        https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/multioutput.py.
+
+        Args:
+            n_estimators (int): Number of classes in the output.
+        """
+
+        self.cc.random_state = check_random_state(self.cc.random_state)
+        self.cc.order_ = self.cc.order
+        if isinstance(self.cc.order_, tuple):
+            self.order_ = np.array(self.order_)
+
+        if self.cc.order_ is None:
+            self.cc.order_ = np.array(range(n_estimators))
+        elif isinstance(self.cc.order_, str):
+            if self.cc.order_ == 'random':
+                self.cc.order_ = self.cc.random_state.permutation(n_estimators)
+        elif sorted(self.order_) != list(range(n_estimators)):
+            raise ValueError("invalid order")
