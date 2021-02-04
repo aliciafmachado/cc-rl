dfe0a189aaf4af59f6232c84703378a225bd21c8
Alicia Fortes Machado <aliciafortesmachado@gmail.com>
2021-02-04 16:57:17 +0100

    Finishing implementation of step and reset

--- src/gym_cc/Env.py
+++ src/gym_cc/Env.py
@@ -6,65 +6,106 @@ import numpy as np
 
 from src.gym_cc.Renderer import Renderer
 
-def import_tree():
-  '''
-
-  '''
-  pass
-
-def import_classifier_chain():
-  '''
-  '''
-  pass
-
-def generate_random_tree():
-  '''
-  '''
-  pass
 
 class Env(gym.Env):
   '''
+  Classifier chains are there and you have to defeat them by 
+  finding the greatest joint probability among all the possible ones
+  The environment receives a classifier chain in the constructor which is
+  used as the predictor
+  The actions corresponds to the choice to go left (0) or right(1) in the
+  tree incurred by the classifier chain
+  You only receive a reward in the end and it corresponds to the final 
+  joint probability
   '''
-  def __init__(self, classifier_chain):
+  def __init__(self, classifier_chain, dataset, random_seed=42):
+    '''
+    Environment constructor
+    Args:
+      classifier_chain : Classifier Chain used in the environment
+      dataset : the dataset that we are working with
+      random_seed : a random_seed for reproducibility
+    '''
+    # Passing the seed
+    np.random.seed(random_seed)
+
     self.classifier_chain = classifier_chain
     self.action_space = spaces.Discrete(2)
     self.observation_path_space = spaces.MultiDiscrete(np.ones((classifier_chain.n_labels,), dtype=int) * 2)
     self.observation_probabilities_space = spaces.Box(low=0, high=1, shape=(classifier_chain.n_labels,), dtype=np.float16)
 
-    self.path = np.zeros((classifier_chain.n_labels,), dtype=bool)
-    self.probabilities = np.zeros((classifier_chain.n_labels,), dtype=float)
+    self.path = np.zeros((classifier_chain.n_labels - 1,), dtype=int)
+    self.probabilities = np.zeros((classifier_chain.n_labels - 1,), dtype=float)
+    self.obs
     self.current_estimator = 0
     self.current_probability = 1
+    self.dataset = dataset
+    self.x = self.dataset.train_x(np.random.randint(0, len(self.dataset.train_x)))
 
     self.renderer = Renderer('print', self.observation_path_space, self.observation_probabilities_space)
 
-  def _take_action(self, action):
-    '''
-    Take the action in the environment
-    '''
-    # TODO
 
-  def _next_observation(self):
+  def _next_observation(self, action):
     '''
-    Return the observation
+    Return the new observation
     '''
-    # TODO
-    return obs
-
-  def step(self, action):
-    # Execute the action
-    self._take_action(action)
-
-    if self.current_step == self.classifier_chain.n_labels:
-      return self.next_observation(), self.current_probability, True, {} 
+    if self.current_estimator > 0:
+      xy = np.append(self.x, self.path[:self.current_estimator])
 
     else:
-      return None, 0, False, {}  
+      xy = self.x 
+
+    obs = self.cc.estimators_[self.current_estimator].predict_proba(xy)
+    
+    self.current_estimator += 1
+
+    return obs
+
+
+  def step(self, action):
+    '''
+    Step in the environment
+    Args:
+      action : Classifier Chain used in the environment
+    Returns:
+      Next left probability
+      The action history
+      The chosen probabilities history
+      Reward
+      If the environment is done: if we arrived in the end of the
+      classifier chain
+    '''
+    # Execute the action
+    if self.current_step == self.classifier_chain.n_labels:
+      self.current_probability *= self.obs[action]
+      return self.obs, self.path, self.probabilities, self.current_probability, True 
+
+    else:
+      # append last observation
+      if self.current_estimator > 0:
+        self.path[self.current_estimator - 1] = action
+
+        # Passing left probability
+        self.probabilities[self.current_estimator - 1] = self.obs[0]
+        self.current_probability *= self.obs[action]
+
+      self.obs = self._next_observation(action)
+      
+      return self.obs, self.path, self.probabilities, 0, False
+
 
   def reset(self):
+    '''
+    Resets the environment
+    '''
     self.current_probability = 1
-    self.current_step = 0
-    # TODO
+    self.current_estimator = 0
+    self.path = np.zeros((classifier_chain.n_labels - 1,), dtype=int)
+    self.probabilities = np.zeros((classifier_chain.n_labels - 1,), dtype=float)
+    self.renderer.reset()
+
+    # We reset x as well
+    self.x = self.dataset.train_x(np.random.randint(0, len(self.dataset.train_x)))
 
   def render(self):
     self.renderer.render()
