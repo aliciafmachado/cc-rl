2536fe34dfe7df24b3658a3b4095938eb440041e
Alicia Fortes Machado <aliciafortesmachado@gmail.com>
2021-02-04 15:57:55 +0100

    Implementing step

--- src/gym-cc/env.py
+++ src/gym-cc/env.py
@@ -22,32 +22,42 @@ def generate_random_tree():
 class Env(gym.Env):
   '''
   '''
-  def __init__(self, classifier_chain=None, raw_tree=None):
-    if classifier_chain != None:
-      desc = import_classifier_chain()
-    else:
-      if raw_tree != None:
-        desc = import_tree()
-      else:
-        desc = generate_random_tree()
-    
+  def __init__(self, classifier_chain):
     self.classifier_chain = classifier_chain
+    self.current_step = 0
+    self.current_probability = 1
     self.action_space = spaces.Discrete(2)
     self.observation_path_space = [spaces.Discrete(2) for e in classifier_chain.n_estimators]
     self.observation_probabilities_space = spaces.Box(low=0, high=1, shape=(classifier_chain.n_estimators,), dtype=np.float16)
 
+  def _take_action(self, action):
+    '''
+    Take the action in the environment
+    '''
+    # TODO
+
+  def _next_observation(self):
+    '''
+    Return the observation
+    '''
+    # TODO
+    return obs
+
   def step(self, action):
-    pass
+    # Execute the action
+    self._take_action(action)
+
+    if self.current_step == self.classifier_chain.n_labels:
+      return self.next_observation(), self.current_probability, True, {} 
+
+    else:
+      return None, 0, False, {}  
 
   def reset(self):
-    pass
+    self.current_probability = 1
+    self.current_step = 0
+    # TODO
+
 
   def render(self, mode='human'):
     pass
-
-
-
-dataset = Dataset(ds)
-cc = ClassifierChain()
-cc.fit(dataset)
-env = Env(cc)
