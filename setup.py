bd197fd88fb3d02aba92128c9572c42552cdc47e
Aloysio <aloysiogl@gmail.com>
2021-02-08 14:38:51 +0100

    Test on importing package

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
 
-setup(name='src', packages=find_packages(),
+print(find_packages())
+
+setup(name='cc-rl', packages=find_packages(),
       version='0.1.0', python_requires='>=3.6')
