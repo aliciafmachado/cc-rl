0b87bb85c23316a25646c554a502a7bee6e41725
Aloysio <aloysiogl@gmail.com>
2021-02-23 10:37:38 +0100

    Create skeleton of MCTS

--- src/cc_rl/rl/MCTSAgent.py
+++ src/cc_rl/rl/MCTSAgent.py
@@ -13,14 +13,55 @@ class MCTSAgent(Agent):
     MCTS to find the best tree path
     """
 
-    def __init__(self, environment: Env):
+    def __init__(self, environment: Env,
+                 c_puct: float = 0.6,
+                 mcts_passes: int = 100):
         super().__init__(environment)
+        self.model = MCTSModel(environment.classifier_chain.n_labels + 1, self.device)
+        self.c_puct = c_puct
+        self.mcts_passes = mcts_passes
 
     def train(self, *args):
         raise NotImplementedError
 
     def __experience_environment(self, *args):
         raise NotImplementedError
+
+    def __experience_environment_once(self):
+        """
+        Goes through a path until the end once using MCTS at each step
+        :returns:
+        """
+        # Each path should go until the end
+        depth = self.environment.classifier_chain.n_labels
+
+        # Get current state from environment
+        next_proba, action_history, proba_history = self.environment.reset()
+
+        # Parameters used for the MCTS
+        N = {} # dictionary that stores the number of times we took action a from state s
+        Q = {} # action-value function
+        P = {}
+
+
+
+
+
+
+        # TODO must return the history of actions probas, next probas adn final values and policies
+        return None
+
+    def __MCTS_pass(self, initial_depth, total_depth, N, Q):
+        for i in range(self.mcts_passes):
+            next_proba, action_history, proba_history = self.environment.reset(initial_depth)
+
+            for j in range(total_depth-initial_depth):
+                # Getting prediction from the
+
+
+                # Getting the next state from environment
+                next_proba, action_history, proba_history, final_value, end = \
+                    self.environment.step(next_action)
 
     def __train_once(self, *args):
         raise NotImplementedError
